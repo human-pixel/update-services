9161c906797803b6a6a282f6adae56957cc8d48b
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-06 15:31:05 +0000

    - 3.1+ compatibility update.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/update-services@2023 f53fe46e-8c02-459b-9fdd-93e75771b278

--- update-services.php
+++ update-services.php
@@ -1,11 +1,12 @@
 <?php
 /*
 Plugin Name: Update Services
-Plugin URI: 
-Description:
-Author: Andrew Billits (Incsub)
-Version: 1.0.1
-Author URI:
+Plugin URI: http://premium.wpmudev.org/project/update-services
+Description: Multisite automatically removes the update services box from the settings 
+Author: S H Mohanjith (Incsub), Andrew Billits (Incsub)
+Version: 1.0.2
+Author URI: http://premium.wpmudev.org
+Network: true
 */ 
 
 /* 
@@ -32,6 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
 //------------------------------------------------------------------------//
+add_action('init', 'update_services_init');
 add_action('wpmu_options', 'update_services_site_admin_options');
 add_action('update_wpmu_options', 'update_services_site_admin_options_process');
 add_filter('whitelist_options', 'update_services_enable', 99);
@@ -40,6 +42,13 @@ add_action('wpmu_new_blog', 'update_services_default', 1, 1);
 //------------------------------------------------------------------------//
 //---Functions------------------------------------------------------------//
 //------------------------------------------------------------------------//
+function update_services_init() {
+	if ( !is_multisite() )
+		exit( 'The Update Services plugin is only compatible with WordPress Multisite.' );
+		
+	load_plugin_textdomain('update_services', false, dirname(plugin_basename(__FILE__)).'/languages');
+}
+
 function update_services_default($blog_ID) {
 	$default_update_services = stripslashes( get_site_option('default_update_services') );
 	if ( $default_update_services == 'empty' ) {
@@ -80,7 +89,7 @@ function update_services_setting_section_output() {
 if ( get_option('blog_public') ) {
 ?>
 
-<p><label for="ping_sites"><?php _e('When you publish a new post, WordPress automatically notifies the following site update services. For more about this, see <a href="http://codex.wordpress.org/Update_Services">Update Services</a> on the Codex. Separate multiple service <abbr title="Universal Resource Locator">URL</abbr>s with line breaks.') ?></label></p>
+<p><label for="ping_sites"><?php _e('When you publish a new post, WordPress automatically notifies the following site update services. For more about this, see <a href="http://codex.wordpress.org/Update_Services">Update Services</a> on the Codex. Separate multiple service <abbr title="Universal Resource Locator">URL</abbr>s with line breaks.', 'update_services') ?></label></p>
 
 <textarea name="ping_sites" id="ping_sites" class="large-text" rows="3"><?php form_option('ping_sites'); ?></textarea>
 
@@ -88,7 +97,7 @@ if ( get_option('blog_public') ) {
 } else {
 ?>
 
-	<p><?php printf(__('WordPress is not notifying any <a href="http://codex.wordpress.org/Update_Services">Update Services</a> because of your blog\'s <a href="%s">privacy settings</a>.'), 'options-privacy.php'); ?></p>
+	<p><?php printf(__('WordPress is not notifying any <a href="http://codex.wordpress.org/Update_Services">Update Services</a> because of your blog\'s <a href="%s">privacy settings</a>.', 'update_services'), 'options-privacy.php'); ?></p>
 
 <?php
 }
@@ -106,17 +115,25 @@ function update_services_site_admin_options() {
 		}
 	}
 	?>
-		<h3><?php _e('Update Services') ?></h3> 
+		<h3><?php _e('Update Services', 'update_services') ?></h3> 
 		<table class="form-table">
 			<tr valign="top"> 
-				<th scope="row"><?php _e('Default Update Services') ?></th> 
+				<th scope="row"><?php _e('Default Update Services', 'update_services') ?></th> 
 				<td><textarea name="default_update_services" type="text" rows="3" wrap="soft" id="default_update_services" style="width: 95%"/><?php echo stripslashes( $default_update_services ); ?></textarea>
 					<br />
-					<?php _e('Separate multiple service URLs with line breaks. Note that the default updates services will be configured for new blogs only.') ?>
+					<?php _e('Separate multiple service URLs with line breaks. Note that the default updates services will be configured for new blogs only.', 'update_services') ?>
 				</td>
 			</tr>
 		</table>
 	<?php
 }
 
-?>
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
+}
